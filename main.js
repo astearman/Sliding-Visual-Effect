6a3ccd233f3f0031966aa3b1f8b18b828f6330d8
Andy Stearman <andrewtstearman@gmail.com>
2020-07-27 14:31:15 -0400

    added additional sections and altered JS to allow multiple uses

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 
-function scrollAppear() {
-    const introText = document.querySelector('.intro-text');
-    const introPosition = introText.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 2;
+const elements = document.querySelectorAll(".hidden");
+const windowHeight = window.innerHeight / 1.5;
 
-    if(introPosition < screenPosition){
-        introText.classList.add('intro-appear');
+
+scrollAppear = () => {
+  elements.forEach(element => {
+    //   get each element's distance from top of screen & computed height
+    let positionFromTop = element.getBoundingClientRect().top;
+    let elementHeight = parseInt(window.getComputedStyle(element).height);
+
+    // trigger animation on scroll down
+    if (positionFromTop < windowHeight) {
+      element.classList.replace("hidden", "fade-in");
     }
-}
 
-window.addEventListener('scroll', scrollAppear);
+    // re-hide elements after leaving the screen
+    if (
+      positionFromTop > windowHeight * 1.5 ||
+      positionFromTop < -elementHeight
+    ) {
+      element.classList.replace("fade-in", "hidden");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollAppear);
