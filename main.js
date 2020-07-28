04b1624a04bb424116b29764ae456a01e2522e07
Andy Stearman <andrewtstearman@gmail.com>
2020-07-28 15:59:27 -0400

    Added more elements and animations

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const elements = document.querySelectorAll(".hidden");
+const elements = document.querySelectorAll(".hidden, .hidden2, .hidden3");
 const windowHeight = window.innerHeight / 1.5;
 
 
@@ -14,14 +14,57 @@ scrollAppear = () => {
       element.classList.replace("hidden", "fade-in");
     }
 
+    if(positionFromTop < windowHeight) {
+      element.classList.replace("hidden2", "fade-in2");
+    }
+
+    if(positionFromTop < windowHeight) {
+      element.classList.replace("hidden3", "fade-in3");
+    }
+
     // re-hide elements after leaving the screen
     if (
       positionFromTop > windowHeight * 1.5 ||
       positionFromTop < -elementHeight
     ) {
       element.classList.replace("fade-in", "hidden");
+      element.classList.replace("fade-in2", "hidden2");
+      element.classList.replace("fade-in3", "hidden3");
     }
   });
 };
 
 window.addEventListener("scroll", scrollAppear);
+
+
+
+/*
+const text = document.querySelector('.fancy');
+const stringText = text.textContent;
+const splitText = stringText.split("");
+
+text.textContent = "";
+
+for(let i=0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++
+  if(char === splitText.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+*/
+
