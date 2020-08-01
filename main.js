6883c93c71dba580d534b9c36ede59c071355732
Andy Stearman <andrewtstearman@gmail.com>
2020-08-01 09:46:35 -0400

    Added more sections and effects

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const elements = document.querySelectorAll(".hidden, .hidden2, .hidden3, .hidden4");
+const elements = document.querySelectorAll(".hidden, .hidden2, .hidden3, .hidden4, .hidden5");
 const windowHeight = window.innerHeight / 1.5;
 
 
@@ -26,6 +26,10 @@ scrollAppear = () => {
       element.classList.replace("hidden4", "fade-in4");
     }
 
+    if(positionFromTop < windowHeight) {
+      element.classList.replace("hidden5", "fade-in5");
+    }
+
     // re-hide elements after leaving the screen
     if (
       positionFromTop > windowHeight * 1.5 ||
@@ -35,6 +39,7 @@ scrollAppear = () => {
       element.classList.replace("fade-in2", "hidden2");
       element.classList.replace("fade-in3", "hidden3");
       element.classList.replace("fade-in4", "hidden4");
+      element.classList.replace("fade-in5", "hidden5");
     }
   });
 };
@@ -73,3 +78,24 @@ function complete() {
 }
 
 
+const texts = ['all the love you can give   ', 'every day of your life   '];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if(count === texts.length){
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type, 250);
+}());
